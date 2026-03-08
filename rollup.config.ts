159e2249f4669163ccd90d77d38f44b3e6f5be75
polllaris <polllaris@protonmail.com>
2026-03-07 21:49:44 -0500

    change rollup config to typescript

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,22 @@
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import json from "@rollup/plugin-json";
+import typescript from "@rollup/plugin-typescript";
 
 const config = {
-    input: "src/index.js",
+    input: "src/index.ts",
     output: {
         esModule: true,
         file: "dist/index.js",
         format: "es",
         sourcemap: true,
     },
-    plugins: [commonjs(), nodeResolve({ preferBuiltins: true }), json()],
+    plugins: [
+        nodeResolve({ preferBuiltins: true }),
+        commonjs(),
+        json(),
+        typescript()
+    ],
 };
 
 export default config;
